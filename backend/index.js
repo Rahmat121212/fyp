b836e1c71feded6db96052245a8559d58778a4d3
Rahmat ullah <baheer224@gmail.com>
2023-05-27 13:54:55 +0500

    feat: configure cors in backend

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,14 @@ const ENV = require("./config/ENV");
 const app = express();
 const connect = require("./config/DB_Connection");
 const admin_router = require("./routes/userRoutes");
+const cors = require('cors');
 connect();
 const PORT = ENV.PORT || 5000;
 app.get("/",(req,res)=>{
     res.send("well Come To office")
 })
 
+app.use(cors());
 app.use(express.json());
 app.use(admin_router)
 
